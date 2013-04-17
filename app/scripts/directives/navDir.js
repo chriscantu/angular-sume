81f93392c0906b3e9754ebce681fd7be4d03d564
Chris Cantu <christopher.cantu@gmail.com>
2013-04-16 23:00:17 -0500

    Added highlight for the active nav selection

--- app/scripts/directives/navDir.js
+++ app/scripts/directives/navDir.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('navDirective', []).
-	directive('navBar', function (){
+	directive('navBar', function ($location){
 		return {
 			restrict: 'E',
 			replace: true,
-			templateUrl: 'directives/navBar.html'
+			templateUrl: 'directives/navBar.html',
+			link: function(scope, element, attrs){
+				console.log($location.path())
+				scope.path = $location.path();
+			}
 		}
 	});
